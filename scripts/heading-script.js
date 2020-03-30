6c2241ee6a95d5dff6ea5ce8b382b72475cec6e7
Morgan Webb <mnbwebb2233@gmail.com>
2020-03-30 21:43:22 +1100

    Improve ghost-typing

--- scripts/heading-script.js
+++ scripts/heading-script.js
@@ -4,17 +4,32 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function random_hex_color_rgb() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 async function ghost_type() {
-    var max = 300;
-    var min = 100;
     var elem = document.getElementById("scripted-text");
     elem.textContent = null;
     for(char of str) {
         elem.textContent += char;
         if(char != ' ') {
-            await sleep(Math.floor(Math.random() * (max - min) + min));
+            await sleep(50);
+        } else {
+            elem.style.color = random_hex_color_rgb();
+        }
+
+        if(elem.textContent == "Hello, World!") {
+            // give it a little... anticipation
+            await sleep(500);
         }
     }
+    elem.style.color = "#FFF";
 }
 
 ghost_type();
