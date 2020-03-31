87ccacb15f4a954cd87e83661eb9919c8db1359f
Morgan Webb <mnbwebb2233@gmail.com>
2020-03-31 12:14:06 +1100

    Projects page

--- scripts/heading-script.js
+++ scripts/heading-script.js
@@ -1,5 +1,3 @@
-var str = "Hello, World! My name is Morgan.";
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -14,14 +12,38 @@ function random_hex_color_rgb() {
 }
 
 async function ghost_type() {
+    to_type = ""
+    current_doc = window.location.pathname.split("/").slice(-1)[0]
     var elem = document.getElementById("scripted-text");
     elem.textContent = null;
-    for(char of str) {
+
+    switch(current_doc) {
+        case "index.html":
+            to_type = "Hello, World! My name is Morgan.";
+            break;
+        case "projects.html":
+            to_type = "Here are some of my projects!";
+            break;
+        case "about.html":
+            to_type = "You can learn about me here.";
+        break;
+        case "contact.html":
+            to_type = "You can find my contact information here.";
+        break;
+    }
+
+    for(char of to_type) {
         elem.textContent += char;
         if(char != ' ') {
             await sleep(50);
-        } else {
-            elem.style.color = random_hex_color_rgb();
+        } else { // when char is space, generate random text color
+            new_color = random_hex_color_rgb();
+            // prevent text from becoming invisible due to background-color conflict
+            if(new_color == '#240090') {
+                elem.style.color = "#FFF";
+            } else {
+                elem.style.color = new_color;
+            }
         }
 
         if(elem.textContent == "Hello, World!") {
